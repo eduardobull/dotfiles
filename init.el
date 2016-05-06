8081559685809ee58bcf9396fe56436f8af8b6fc
Eduardo Gutierres <eduardo.gutierres@karsten.com.br>
2016-05-06 16:28:12 -0300

    [emacs] Backups config changed

--- init.el
+++ init.el
@@ -36,8 +36,25 @@
 ;;------------------
 ;; Custom config
 
+;; Misc
 (load-theme 'monokai t) ;; Theme
 (linum-mode 1) ;; Line numbers
+
+;; Backups
+(defvar --backup-directory (concat user-emacs-directory "backups"))
+(if (not (file-exists-p --backup-directory))
+    (make-directory --backup-directory t))
+(setq backup-directory-alist `(("." . ,--backup-directory)))
+(setq make-backup-files t               ; backup of a file the first time it is saved.
+      backup-by-copying t               ; don't clobber symlinks
+      version-control t                 ; version numbers for backup files
+      delete-old-versions t             ; delete excess backup files silently
+      kept-old-versions 6               ; oldest versions to keep when a new numbered backup is made (default: 2)
+      kept-new-versions 9               ; newest versions to keep when a new numbered backup is made (default: 2)
+      auto-save-default t               ; auto-save every buffer that visits a file
+      auto-save-timeout 20              ; number of seconds idle time before auto-save (default: 30)
+      auto-save-interval 100            ; number of keystrokes between auto-saves (default: 300)
+      )
 
 ;; Disable parens highlight delay
 (show-paren-mode 0)
