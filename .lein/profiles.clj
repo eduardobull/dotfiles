9e4aeaf63ed770a6c095eaee7ebfcbb799316460
Eduardo Gutierres <eduardo.gutierres@karsten.com.br>
2016-05-13 12:48:06 -0300

    [.lein/profiles.clj] cljfmt

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,8 +1,9 @@
 {:user
- {:plugins
-  [[lein-ancient  "0.6.10"]
-   [refactor-nrepl  "2.2.0"]
-   [cider/cider-nrepl  "0.12.0"]
-   ;[jonase/eastwood  "0.2.3" :exclusions [org.clojure/clojure]]
-   [lein-kibit  "0.1.2"]
-   [venantius/ultra  "0.4.1"]]}}
+ {:dependencies [[cljfmt  "0.5.3"]]
+  :plugins [[lein-ancient "0.6.10"]
+            [lein-cljfmt "0.5.3"]
+            [refactor-nrepl "2.2.0"]
+            [cider/cider-nrepl "0.12.0"]
+            ;[jonase/eastwood  "0.2.3" :exclusions [org.clojure/clojure]]
+            [lein-kibit "0.1.2"]
+            [venantius/ultra "0.4.1"]]}}
