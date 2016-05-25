cfb319eb2196cda3392678c0ba43f8eee5a6154e
Eduardo Gutierres <eduardo.gutierres@karsten.com.br>
2016-05-25 15:26:41 -0300

    Bugfixes

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,8 +2,8 @@
  {:dependencies [[cljfmt  "0.5.3"]]
   :plugins [[lein-ancient "0.6.10"]
             [lein-cljfmt "0.5.3"]
-            [refactor-nrepl "2.2.0"]
             [cider/cider-nrepl "0.12.0"]
+            [refactor-nrepl "2.2.0"]
             ;[jonase/eastwood  "0.2.3" :exclusions [org.clojure/clojure]]
             [lein-kibit "0.1.2"]
             [venantius/ultra "0.4.1"]]}}
