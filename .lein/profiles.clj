c37969e6b64cbdf2594402bd1c32efd9506e8af4
Eduardo Büll <eduardo.bull@gmail.com>
2016-07-02 13:21:07 -0300

    Added pprint lein plugin

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -4,6 +4,7 @@
             [lein-cljfmt "0.5.3"]
             [lein-try  "0.4.3"]
             [lein-kibit "0.1.2"]
+            [lein-pprint  "1.1.1"]
             [jonase/eastwood  "0.2.3"]
             [cider/cider-nrepl "0.12.0"]
             [refactor-nrepl "2.2.0"]
