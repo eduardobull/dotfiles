e1206d0d941ab3da3b56f2013bfe4ed71288368a
Eduardo Gutierres <eduardo.gutierres@karsten.com.br>
2017-04-10 15:59:19 -0300

    [profiles.clj] Added pjstadig.humane-test-output

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,6 +3,9 @@
                   [lein-kibit "0.1.3"]
                   [lein-pprint  "1.1.2"]
                   [jonase/eastwood  "0.2.3"]
-                  [venantius/ultra "0.5.1"]]}
+                  [venantius/ultra "0.5.1"]
+                  [pjstadig/humane-test-output "0.8.1"]]
+        :injections [(require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]}
  :repl {:plugins [[refactor-nrepl "2.3.0"]
                   [cider/cider-nrepl "0.14.0"]]}}
