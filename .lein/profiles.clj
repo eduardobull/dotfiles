3b502e77a85b2c138ff747fbe2b10041596b7663
Eduardo Gutierres <eduardo.gutierres@karsten.com.br>
2017-04-10 17:26:23 -0300

    [profiles.clj] Removed humane-test-output

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,9 +3,6 @@
                   [lein-kibit "0.1.3"]
                   [lein-pprint  "1.1.2"]
                   [jonase/eastwood  "0.2.3"]
-                  [venantius/ultra "0.5.1"]
-                  [pjstadig/humane-test-output "0.8.1"]]
-        :injections [(require 'pjstadig.humane-test-output)
-                     (pjstadig.humane-test-output/activate!)]}
+                  [venantius/ultra "0.5.1"]]}
  :repl {:plugins [[refactor-nrepl "2.3.0"]
                   [cider/cider-nrepl "0.14.0"]]}}
