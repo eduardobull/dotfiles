2411e5a25f1c2d82220a9d296765874be0721993
Eduardo Gutierres <eduardo.gutierres@karsten.com.br>
2017-06-15 13:18:17 -0300

    [init.el] HTML major mode spaces

--- init.el
+++ init.el
@@ -292,6 +292,14 @@
 
 
 ;;------------------
+;; HTML
+
+(add-hook 'html-mode-hook
+          (lambda ()
+            ;; Default indentation is usually 2 spaces, changing to 4.
+            (set (make-local-variable 'sgml-basic-offset) 4)))
+
+;;------------------
 ;; Python
 
 (elpy-enable)
