9bbb22ba268dd53fdb5311a95862cbf150092e96
Eduardo Gutierres <eduardo.gutierres@karsten.com.br>
2017-06-15 10:33:19 -0300

    [profiles.clj] dependencies updated

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,10 +1,10 @@
 {:user {:plugins [[lein-ancient "0.6.10"]
                   [lein-try  "0.4.3"]
-                  [lein-kibit "0.1.3"]
+                  [lein-kibit "0.1.5"]
                   [lein-pprint  "1.1.2"]
-                  [jonase/eastwood  "0.2.3"]
+                  [jonase/eastwood  "0.2.4"]
                   [venantius/ultra "0.5.1"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.12"]
+        :dependencies [[org.clojure/tools.nrepl "0.2.13"]
                        [alembic "0.3.2"]]}
- :repl {:plugins [[refactor-nrepl "2.3.0"]
+ :repl {:plugins [[refactor-nrepl "2.3.1"]
                   [cider/cider-nrepl "0.14.0"]]}}
