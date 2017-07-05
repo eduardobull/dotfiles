cf1360c04c2bb974d8e591c6d32b27e913e24dc4
Eduardo Büll <eduardo.bull@gmail.com>
2017-07-05 20:07:58 -0300

    [.lein]

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,8 +3,14 @@
                   [lein-kibit "0.1.5"]
                   [lein-pprint  "1.1.2"]
                   [jonase/eastwood  "0.2.4"]
-                  [venantius/ultra "0.5.1"]]
+                  [venantius/ultra "0.5.1"]
+                  [com.palletops/lein-shorthand "0.4.0"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.13"]
-                       [alembic "0.3.2"]]}
+                       [alembic "0.3.2"]]
+        :shorthand {. {pp clojure.pprint/pprint
+                       doc clojure.repl/doc
+                       source clojure.repl/source
+                       load-project alembic.still/load-project
+                       add-dep alembic.still/distill}}}
  :repl {:plugins [[refactor-nrepl "2.3.1"]
                   [cider/cider-nrepl "0.14.0"]]}}
